f062f350578787069bf5cb87aa9b0618a97271aa
Anna2791 <annushka279@mail.ru>
2025-03-02 16:56:58 +0300

    ДОБАВИЛА стр
    а

--- flower_shop/telegram_bot.py
+++ flower_shop/telegram_bot.py
@@ -4,6 +4,25 @@ from telegram import Bot
 
 import telebot
 from telebot import types
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'Привет! Я ваш бот для заказа букетов. Чем могу помочь?')
+
+@bot.message_handler(commands=['check'])
+def check_bot(message):
+    bot.reply_to(message, 'Бот работает!')
+
+def send_order_to_telegram(order):
+    message = f'Новый заказ!\nБукет: {order.flower.name}\nЦена: {order.flower.price} руб.'
+    try:
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+        print("Сообщение успешно отправлено")
+    except Exception as e:
+        print(f"Ошибка при отправке: {e}")
+
+
 def send_order_to_telegram(order):
     bot = Bot(token=TELEGRAM_BOT_TOKEN)
     message = f'Новый заказ!\nБукет: {order.flower.name}\nЦена: {order.flower.price} руб.'
@@ -13,3 +32,5 @@ def send_order_to_telegram(order):
     except Exception as e:
         print(f"Ошибка при отправке: {e}")
 
+if __name__ == '__main__':
+    bot.polling()
