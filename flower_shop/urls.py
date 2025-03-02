dc729fa491c9dc51b3345452d26c1135e54795b3
Anna2791 <annushka279@mail.ru>
2025-03-02 17:19:28 +0300

    чот
    а

--- flower_shop/urls.py
+++ flower_shop/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from flower_shop import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('catalog/', views.catalog, name='catalog'),
+    path('', views.catalog, name='catalog'),
     path('order/<int:flower_id>/', views.order, name='order'),
+    path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
-    # Можно добавить путь по умолчанию:
-    path('', views.catalog, name='home'),
 ]
+
+
